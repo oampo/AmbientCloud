e68bcc26492f91340fa46e5c471eecc04892d983
Joe Turner <joe@oampo.co.uk>
2012-02-06 12:37:11 +0000

    Add oauth callback file, and initial connection methods

---
+++ js/Main.js
@@ -0,0 +1,9 @@
+var SlowCloud = function() {
+};
+
+window.onload = function() {
+    SC.initialize({
+        client_id: '382b4cea1549fc6ed4682acaf0e0fe65',
+        redirect_uri: 'http://slooowcloud.nodejitsu.com/callback.html',
+    });
+};
